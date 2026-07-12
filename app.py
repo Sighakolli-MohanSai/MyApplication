073da4072ff2cd9a0531e3699332736ea72f89d7
Sighakolli-MohanSai <140317493+Sighakolli-MohanSai@users.noreply.github.com>
2026-07-12 17:23:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, session
 
 from auth.login import login_bp
 from auth.register import register_bp
@@ -9,6 +9,18 @@ app.secret_key = "movie_theatre_secret_key"
 
 app.register_blueprint(login_bp)
 app.register_blueprint(register_bp)
+
+
+@app.route("/theatre")
+def theatre():
+
+    return render_template(
+        "theatre.html",
+        firstname=session.get("firstname"),
+        lastname=session.get("lastname"),
+        movie=session.get("movie")
+    )
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
